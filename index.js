40f789f49c269988efabaa7a9ee43d7489b883eb
EmazAkhtar <emazakhtar11@gmail.com>
2023-09-07 22:28:30 +0530

    webhook endpoint added

--- index.js
+++ index.js
@@ -29,29 +29,57 @@ async function main() {
 }
 
 // WebHook...
+
+// Replace this endpoint secret with your endpoint's unique secret
+// If you are testing with the CLI, find the secret by running 'stripe listen'
+// If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+// at https://dashboard.stripe.com/webhooks
+const endpointSecret = "whsec_xWd5waJuVhNel0M1i0FfuZOWdVyodUN9";
+
 app.post(
   "/webhook",
   express.raw({ type: "application/json" }),
   (request, response) => {
-    const event = request.body;
+    let event = request.body;
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers["stripe-signature"];
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
 
     // Handle the event
     switch (event.type) {
       case "payment_intent.succeeded":
         const paymentIntent = event.data.object;
-        console.log("PaymentIntent was successful!", paymentIntent);
+        console.log(
+          `PaymentIntent for ${paymentIntent.amount} was successful!`
+        );
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
         break;
       case "payment_method.attached":
         const paymentMethod = event.data.object;
-        console.log("PaymentMethod was attached to a Customer!");
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
         break;
-      // ... handle other event types
       default:
-        console.log(`Unhandled event type ${event.type}`);
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
     }
 
     // Return a 200 response to acknowledge receipt of the event
-    response.json({ received: true });
+    response.send();
   }
 );
 
